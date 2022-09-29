251b6b4229ba8f93be3ef99f4854fe3385638795
Shipan <liushipan1998@gmail.com>
2022-09-29 18:02:12 +0200

    29.09.2022: REST_CRUD finished

--- m5-practice_REST_CRUD/src/main/java/com/atguigu/controller/EmpController.java
+++ m5-practice_REST_CRUD/src/main/java/com/atguigu/controller/EmpController.java
@@ -7,10 +7,7 @@ import com.atguigu.pojo.Employee;
 import jdk.nashorn.internal.objects.annotations.Property;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.Map;
@@ -68,6 +65,50 @@ public class EmpController {
         /* after saving, getAll again and redirect to getAllEmps() and will show the the emp_list_html again  page*/
         /*when you want redirect to a html page, you have to write "redirect:/new_page.html", please do not forget the .html*/
         return "redirect:/emps"; // redirect to getAllEmps()
+    }
+
+
+    /*
+    *
+    * delete
+    *
+    * */
+
+    @DeleteMapping("/emps/{empId}")
+    //@PathVariable("empId") Integer id, get the variable from the URL and map it to the:  Integer id
+    public String deleteEmp(@PathVariable("empId") Integer id) {
+        // delete this emp
+        empDao.delete(id);
+        return "redirect:/emps";  //  go back to emp_list
+    }
+
+    /*
+    *
+    *
+    * 跳转到  modify 页面
+    *先把所有的 页面查出来
+    *
+    * */
+
+    @GetMapping("/toUpdateEmp/{empId}")
+    public String toUpdateEmp(@PathVariable("empId") Integer id, Map<String,Object> map) {
+        Employee emp = empDao.get(id);
+        map.put("emp", emp);
+        Collection<Department> depts = depDao.getDeps();
+        map.put("depts", depts);
+        return "emp_update";
+    }
+
+
+
+    /*  modify 的功能书写  */
+
+    @PutMapping("/emps")
+    public String updateEmp(Employee emp) { // form 提交的 数据会 自动生成 Employee 格式， 然后 pojo 入参。
+        // 调用修改
+        System.out.println(emp);
+        empDao.save(emp);
+        return "redirect:/emps";  // 修改完之后， 也要查一下。 go to getAllEmps()
     }
 
 
